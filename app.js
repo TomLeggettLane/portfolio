6886f144829d772baa7dc79419e6999b7b1f27b8
tom <tleg081@aucklanduni.ac.nz>
2021-12-28 14:45:08 +1300

    animations working, carousel working

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
-$(".nav-item a").on("click", function() {
-    $(".nav-item a").removeClass("active");
-    $(this).addClass("active");
-    console.log("clicked!");
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-main',
   });
 
 
-$('body').scrollspy({target: "#navbar-main"})
+$(function () {
+    var isAnimatedScroll = false;
+    $(window).scroll(function () {
+        setupVisibleElementAnimations();
+    });
+
+    $(window).on("load");
+
+    function setupVisibleElementAnimations() {
+        var $els = $("[data-animation]");
+        
+        $els.each(function () {
+            var $el = $(this);
+
+            if ($el.hasClass("animated")) {
+                return;
+            } else if (!$el.hasClass("animated") && isElementInView($el)) {
+                var animationClass = $el.attr("data-animation");
+                var animationDelay = $el.attr('data-delay');
+                $el.css({
+                    "-webkit-animation-delay": animationDelay,
+                    "-moz-animation-delay": animationDelay,
+                    "animation-delay": animationDelay
+                });
+                $el.addClass("animated").addClass(animationClass);
+                $el.removeClass("invis");
+            }
+        });
+    }
+
+    function isElementInView(el) {
+        if (typeof jQuery === "function" && el instanceof jQuery) {
+            el = el[0];
+        }
+
+        var rect = el.getBoundingClientRect();
+
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && 
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+    });
